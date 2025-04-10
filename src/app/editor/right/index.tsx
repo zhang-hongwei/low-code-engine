35c3e71e5814baa2cad6410d24df69d66e0d9ee8
张红伟 <467666623@qq.com>
2025-04-10 22:37:06 +0800

    feat: 1

--- src/app/editor/right/index.tsx
+++ src/app/editor/right/index.tsx
@@ -1,26 +1,44 @@
 import { useEditorStore } from "@/store/editor";
 import { GridItemPropsPanel } from "./pannel/GridItemPropsPanel";
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+const labelData = [
+    { label: '弹性布局(flex)', value: 'flex' },
+    { label: '栅格布局(grid)', value: 'grid' },
+];
 
 const Center = () => {
     const activeId = useEditorStore(state => state.activeId);
     const nodeMap = useEditorStore(state => state.componentTree.nodeMap);
+    const updateNodeProps = useEditorStore(state => state.updateNodeProps);
 
     const node = activeId ? nodeMap[activeId] : null;
 
     if (!node) return <div className="p-4 text-gray-400">未选中组件</div>;
 
-    const handleChange = (key: string, value: any) => {
-        // setComponentProps(node.id, {
-        //     ...node.props,
-        //     [key]: value,
-        // });
+    const handleChange = (event: any, item: any) => {
+        console.log(event, item.value)
+        if (!activeId) return;
+        updateNodeProps(activeId, {
+            style: {
+
+                'display': item.value,
+            },
+        });
     };
 
 
 
     return (
         <div className="p-4 border-l w-[300px] bg-white">
-
+            <Autocomplete
+                disablePortal
+                options={labelData}
+                sx={{ width: 300 }}
+                renderInput={(params) => <TextField {...params} label="布局方式" />}
+                onChange={handleChange}
+            />
             <GridItemPropsPanel />
         </div>
     );
